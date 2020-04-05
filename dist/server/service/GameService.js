de208a7412d7d45753be050f09356010ce52b356
Vasia Patov <vasiapatov@gmail.com>
2020-04-05 15:22:03 -0400

    Remove dist from remote

--- dist/server/service/GameService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class GameService {
-}
-exports.GameService = GameService;
-//# sourceMappingURL=GameService.js.map
--- dist/server/util/util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function generateUUID() {
-    return Math.random().toString(36).substring(2, 15) +
-        Math.random().toString(36).substring(2, 15);
-}
-exports.generateUUID = generateUUID;
-//# sourceMappingURL=util.js.map
