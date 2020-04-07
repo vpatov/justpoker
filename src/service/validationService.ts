3044e73d8019788ba1edaf28a3a6dce87707c2fa
Vasia Patov <vasiapatov@gmail.com>
2020-04-07 03:47:32 -0400

    Refactors, validations, and extra logic. Two players can now check the preflop (lol)

--- src/service/validationService.ts
+++ src/service/validationService.ts
@@ -1,6 +1,15 @@
 import { Service } from "typedi";
 import { GameStateManager } from './gameStateManager';
-import { SitDownRequest } from '../models/wsaction';
+import { BettingRoundActionType, BettingRoundAction } from '../models/game';
+import { SitDownRequest, JoinTableRequest } from '../models/wsaction';
+
+/*
+    TODO: Redesign error message construction
+    TODO: Split methods into validateAction and ensureCondition
+        - validateAction methods called externall, ensureCondition methods called internally
+*/
+
+const MAX_NAME_LENGTH = 32;
 
 @Service()
 export class ValidationService {
@@ -9,47 +18,73 @@ export class ValidationService {
         private readonly gameStateManager: GameStateManager,
     ) { }
 
-    validateClientExists(clientUUID: string) {
+    ensureClientExists(clientUUID: string) {
         const client = this.gameStateManager.getConnectedClient(clientUUID);
         if (!client) {
             throw Error(`Client ${clientUUID} does not exist.`);
         }
-
-        return client;
     }
 
-    validateClientIsInGame(clientUUID: string) {
-        const client = this.validateClientExists(clientUUID);
-
+    ensureClientIsInGame(clientUUID: string) {
+        const client = this.gameStateManager.getConnectedClient(clientUUID);
         if (!client.playerUUID) {
             throw Error(`Client ${clientUUID} has not joined the game.`);
         }
 
         const player = this.gameStateManager.getPlayer(client.playerUUID);
-
         if (!player) {
             throw Error(`Player ${client.playerUUID} does not exist.`);
         }
-
-        return player;
     }
 
-    validateClientIsNotInGame(clientUUID: string) {
-        const client = this.validateClientExists(clientUUID);
-
+    ensureClientIsNotInGame(clientUUID: string) {
+        const client = this.gameStateManager.getConnectedClient(clientUUID);
         if (client.playerUUID) {
             throw Error(`Client ${clientUUID} already has player association: ${client.playerUUID}`);
         }
     }
 
-    validateSitDownAction(clientUUID: string, request: SitDownRequest) {
+    ensurePlayerIsSitting(playerUUID: string) {
+        const player = this.gameStateManager.getPlayer(playerUUID);
+        if (!player.sitting) {
+            throw Error(`Player is not sitting:\nplayerUUID: ${player.uuid}\nname: ${player.name}\n`);
+        }
+    }
 
-        this.validateClientIsInGame(clientUUID);
+    ensurePlayerIsStanding(playerUUID: string) {
+        const player = this.gameStateManager.getPlayer(playerUUID);
+        if (player.sitting) {
+            throw Error(`Player is not standing:\nplayerUUID: ${player.uuid}\nname: ${player.name}\n`);
+        }
+    }
+
+    // TODO can probably use string templates in a more elegant way to handle errorPrefix
+    ensureCorrectPlayerToAct(clientUUID: string) {
+        this.ensureClientIsInGame(clientUUID);
+        const player = this.gameStateManager.getPlayerByClientUUID(clientUUID);
+        this.ensurePlayerIsSitting(player.uuid);
+        const currentPlayerToAct = this.gameStateManager.getCurrentPlayerToAct();
+        if (player.uuid !== currentPlayerToAct) {
+            throw Error(`Not your turn!\nplayerUUID: ${player.uuid}\nname: ${player.name}\n`);
+        }
+    }
+
+    validateJoinTableRequest(clientUUID: string, request: JoinTableRequest) {
+        this.ensureClientIsNotInGame(clientUUID);
+        if (request.name.length > MAX_NAME_LENGTH) {
+            throw Error(`Name ${request.name} is too long - exceeds limit of 32 characters.`);
+        }
+    }
+
+
+    validateSitDownRequest(clientUUID: string, request: SitDownRequest) {
+        this.ensureClientIsInGame(clientUUID);
 
         const seatNumber = request.seatNumber;
         const player = this.gameStateManager.getPlayerByClientUUID(clientUUID);
-
         const errorPrefix = `Cannot SitDown\nplayerUUID: ${player.uuid}\nname: ${player.name}\n`;
+
+        this.ensurePlayerIsStanding(player.uuid);
 
         if (player.chips <= 0) {
             throw Error(`${errorPrefix} Player needs chips to sit down.`);
@@ -64,28 +99,37 @@ export class ValidationService {
         }
     }
 
-    validateStandUpAction(clientUUID: string) {
-        this.validateClientIsInGame(clientUUID);
-
+    validateStandUpRequest(clientUUID: string) {
+        this.ensureClientIsInGame(clientUUID);
         const player = this.gameStateManager.getPlayerByClientUUID(clientUUID);
-        const errorPrefix = `Cannot StandUp\nplayerUUID: ${player.uuid}\nname: ${player.name}\n`;
-
-        if (!player.sitting) {
-            throw Error(`${errorPrefix} Player is already standing.`)
-        }
+        this.ensurePlayerIsSitting(player.uuid);
     }
 
-    validateStartGameAction(clientUUID: string) {
+    validateStartGameRequest(clientUUID: string) {
         // TODO validate that client is admin
         if (this.gameStateManager.isGameInProgress()) {
             throw Error(`Cannot StartGame: Game is already in progress.`);
         }
     }
 
-    validateStopGameAction(clientUUID: string) {
+    validateStopGameRequest(clientUUID: string) {
         // TODO validate that client is admin
         if (!this.gameStateManager.isGameInProgress()) {
             throw Error(`Cannot StopGame: Game is not in progress.`);
+        }
+    }
+
+
+    validateCheckAction(clientUUID: string) {
+        this.ensureCorrectPlayerToAct(clientUUID);
+
+        const player = this.gameStateManager.getPlayerByClientUUID(clientUUID);
+        const bettingRoundActions = this.gameStateManager.getBettingRoundActions();
+
+        for (const action of bettingRoundActions) {
+            if (action.type === BettingRoundActionType.BET) {
+                throw Error(`You cannot check when someone before you has bet.`);
+            }
         }
     }
 
