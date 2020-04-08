5dfe0f5f6fa26f39ed1f1e41166fd38c62b1356e
Vasia Patov <vasiapatov@gmail.com>
2020-04-08 02:21:33 -0400

    Successfully combined configs for ui/server into one repo

--- merge.js
+++
@@ -1,6 +0,0 @@
-var merge = require('package-merge')
-var fs = require('fs');
-
-var dst = fs.readFileSync('package.json');
-var src = fs.readFileSync('package2.json');
-fs.writeFile("./newPackage.json", merge(dst,src));
