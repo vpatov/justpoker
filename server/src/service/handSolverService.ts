c9e025482933970fc2ced574aaba69321efeb775
Vasia Patov <vasiapatov@gmail.com>
2020-04-13 23:08:36 -0400

    Finished betting logic, multiple all ins, multi way pots. Added timer to showdown such that each side pot shows winner. Need to work out kinks around timing

--- server/src/service/handSolverService.ts
+++ server/src/service/handSolverService.ts
@@ -2,18 +2,23 @@ import { Service } from 'typedi';
 import { Card, SUIT_ABBREVIATIONS } from '../../../shared/models/cards';
 import { Hand as HandSolver } from 'pokersolver';
 
+// TODO declare types for this module and make an npm package
+export declare interface Hand {
+    name: string;
+}
+
 @Service()
 export class HandSolverService {
-    computeBestHandFromCards(cards: Card[]) {
+    computeBestHandFromCards(cards: Card[]): Hand {
         const strCards = cards.map((card) => `${card.rank}${SUIT_ABBREVIATIONS[card.suit]}`);
         return this.computeBestHandFromStrCards(strCards);
     }
 
-    computeBestHandFromStrCards(cards: string[]) {
+    computeBestHandFromStrCards(cards: string[]): Hand {
         return HandSolver.solve(cards);
     }
 
-    getWinningHands(hands: any[]) {
+    getWinningHands(hands: Hand[]): Hand[] {
         return HandSolver.winners(hands);
     }
 }
--- server/src/service/timerManager.ts
+++ server/src/service/timerManager.ts
@@ -1,29 +1,42 @@
 import { Service } from 'typedi';
 import { Subject } from 'rxjs';
+import { GameState } from '../../../shared/models/gameState';
 
 @Service()
 export class TimerManager {
     private gameTimer: NodeJS.Timer;
-    private updateEmitter: Subject<void> = new Subject<void>();
+    private updateEmitter: Subject<GameState> = new Subject<GameState>();
 
-    globalTimerFn(fn: Function, bindScope: any, updateEmitter: Subject<void>) {
+    globalTimerFn(
+        fn: Function,
+        bindScope: any,
+        updateEmitter: Subject<GameState>,
+        getUpdateScope: any,
+        getUpdate: () => GameState,
+    ) {
+        debugger;
         fn.bind(bindScope)();
-        updateEmitter.next();
+        updateEmitter.next(getUpdate.bind(getUpdateScope)());
     }
 
     observeUpdates() {
         return this.updateEmitter.asObservable();
     }
 
-    //TODO clear existing timers
+    // TODO clear existing timers
     // TODO when end of round timer is activated, server should not accept any more turn to act state changes
-    setTimer(bindScope: any, fn: Function, timeout: number) {
-        this.gameTimer = global.setTimeout(
+    // TODO cleaner design - instead of passing getUpdateFn, you could just pass the getUpdateScope, which will
+    //      be expected to have a getUpdate function. (would that be cleaner)?
+    setTimer(fnScope: any, fn: Function, getUpdateScope: any, getUpdateFn: () => GameState, timeout: number) {
+        console.log('setTimer', fnScope, fn, getUpdateScope, getUpdateFn, timeout);
+        global.setTimeout(
             this.globalTimerFn /* Timer manager function that executes fn */,
             timeout /* Timeout value in ms */,
             fn /* Function passed in by caller of setTimer, contains game logic */,
-            bindScope /* bindScope is the object that called setTimer. Required to resolve "this" inside fn */,
+            fnScope /* fnScope is the object that called setTimer. Required to resolve "this" inside fn */,
             this.updateEmitter /* Subject to emit after function execution is complete */,
+            getUpdateScope /* getUpdateScope is the scope used to execute the getUpateFn*/,
+            getUpdateFn /* Function that returns value that should be emitted */,
         );
     }
 }
