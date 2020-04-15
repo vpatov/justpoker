4e94c4efed1aec94b0535ac0b006a1d11b280052
jberman49 <jberman49@bloomberg.net>
2020-04-14 23:21:34 -0400

    add spinner

--- ui/src/Game.tsx
+++ ui/src/Game.tsx
@@ -4,27 +4,27 @@ import Table from "./Table";
 import Controller from "./Controller";
 import { UiGameState } from "./shared/models/uiState";
 const useStyles = makeStyles((theme) => ({
-  root: {
-    height: "100vh",
-    width: "100vw",
-    ...theme.custom.BACKGROUND,
-  },
+    root: {
+        height: "100vh",
+        width: "100vw",
+        ...theme.custom.BACKGROUND,
+    },
 }));
 
 export interface GameProps {
-  game: UiGameState;
+    game: UiGameState;
 }
 
 function Game(props: GameProps) {
-  const classes = useStyles();
-  const { table, controller } = props.game;
+    const classes = useStyles();
+    const { table, controller, heroInGame } = props.game;
 
-  return (
-    <div className={classes.root}>
-      <Table table={table} />
-      <Controller controller={controller} />
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+            <Table table={table} heroInGame={heroInGame} />
+            <Controller controller={controller} />
+        </div>
+    );
 }
 
 export default Game;
