43d6fa9a06fd698e2a16f6d83690110f681c6c07
jberman49 <jberman49@bloomberg.net>
2020-04-17 19:38:57 -0400

    fix bug with player timer not being reset when receive new props on fe

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -51,7 +51,7 @@ class Server {
         private gsm: GameStateManager,
         private stateTransformService: StateTransformService,
         private timerManager: TimerManager,
-    ) { }
+    ) {}
 
     private initRoutes(): void {
         const router = express.Router();
@@ -122,20 +122,22 @@ class Server {
             console.log('connected to ip:', ip);
 
             let clientID = '';
-            try {
-                // try to get clientID from cookie (local script testing)
-                clientID = cookie.parse(req.headers.cookie).clientID;
-                // try to get clientID from url (frontend)
-                if (!clientID) {
-                    const regEx = /clientID\=(\w+)/g;
-                    clientID = Array.from(req.url.matchAll(regEx), (m) => m[1])[0];
-                }
-                if (!clientID) {
-                    clientID = generateUUID();
-                }
-            } catch (e) {
+            // try to get clientID from cookie (local script testing)
+            // try {
+            //     clientID = cookie.parse(req.headers.cookie).clientID;
+            // } catch (e) {
+            //     console.log(e, 'generating new uuid');
+            // }
+
+            // try to get clientID from url (frontend)
+            if (!clientID) {
+                const regEx = /clientID\=(\w+)/g;
+                clientID = Array.from(req.url.matchAll(regEx), (m) => m[1])[0];
+            }
+            if (!clientID) {
                 clientID = generateUUID();
             }
+
             console.log('clientID: ', clientID);
 
             // TODO server shouldnt be communicating with the gameStateManager, but with some
@@ -144,7 +146,6 @@ class Server {
 
             ws.send(
                 JSON.stringify({
-                    status: 200,
                     clientID,
                 }),
             );
--- ui/src/PlayerTimer.tsx
+++ ui/src/PlayerTimer.tsx
@@ -6,60 +6,64 @@ import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         linearTimer: {
-            width: '100%',
-
-
+            width: "100%",
         },
         linearTimerRoot: {
             borderBottomLeftRadius: "0.6vmin",
             borderBottomRightRadius: "0.6vmin",
 
-            height: '0.5vmin',
+            height: "0.5vmin",
         },
     })
 );
 
-function getPercetTimeRemaining(timeElapsed: number, timeLimit: number): number {
-
-    const timeElapsedPercentage = timeElapsed * 100.0 / timeLimit
-    return 100.0 - timeElapsedPercentage
-
+function getPercetTimeRemaining(
+    timeElapsed: number,
+    timeLimit: number
+): number {
+    const timeElapsedPercentage = (timeElapsed * 100.0) / timeLimit;
+    return 100.0 - timeElapsedPercentage;
 }
 
 function PlayerTimer(props) {
     const classes = useStyles();
     const { playerTimer, className } = props;
-    const { timeLimit, timeElapsed } = playerTimer
+    const { timeLimit, timeElapsed } = playerTimer;
 
     const [completed, setCompleted] = useState(100.0);
     const [timer, setTimer] = useState();
 
-
     useEffect(() => {
-        setCompleted(getPercetTimeRemaining(timeElapsed, timeLimit))
-        const updateIntervalMs = 250
-        const reduceBy = (100.0 / timeLimit) * (updateIntervalMs / 1000)
+        setCompleted(getPercetTimeRemaining(timeElapsed, timeLimit));
+        const updateIntervalMs = 250;
+        const reduceBy = (100.0 / timeLimit) * (updateIntervalMs / 1000);
         function progress() {
-            setCompleted((oldCompleted) => oldCompleted - reduceBy)
+            setCompleted((oldCompleted) => oldCompleted - reduceBy);
         }
         const timer = setInterval(progress, updateIntervalMs);
-        setTimer(timer as any)
+        setTimer(timer as any);
         return () => {
             clearInterval(timer);
         };
-    }, []);
-
+    }, [timeElapsed, timeLimit]);
 
     if (completed <= 0) {
         clearInterval(timer);
     }
 
-
-    return (<div className={className} >
-        <LinearProgress color='secondary' variant="determinate" value={completed} className={classes.linearTimer} classes={{
-            root: classes.linearTimerRoot
-        }} />
-    </div>)
+    return (
+        <div className={className}>
+            <LinearProgress
+                color="primary"
+                variant="determinate"
+                value={completed}
+                className={classes.linearTimer}
+                classes={{
+                    root: classes.linearTimerRoot,
+                }}
+            />
+        </div>
+    );
 }
 
 export default PlayerTimer;
