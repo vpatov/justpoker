acf48101e247c67f088cbf522d7b49216044c5f8
jberman49 <jberman49@bloomberg.net>
2020-04-17 13:19:24 -0400

    fix bug where new websocket for prev connect client is not set into client state memory, causing client refresh connections to not work

--- ui/src/shared/models/table.ts
+++ ui/src/shared/models/table.ts
@@ -15,33 +15,33 @@ import WebSocket from "ws";
 */
 
 export declare interface Table {
-  // gameState: GameState;
+    // gameState: GameState;
 
-  readonly uuid: string;
+    readonly uuid: string;
 
-  // map the same person (by cookie) to the same connected client
-  // this mapping is established upon their first connection to the table,
-  // and for the host, established upon table creation.
-  readonly activeConnections: Map<string, ConnectedClient>;
-  // acitveConnections: { [key: string]: ConnectedClient };
+    // map the same person (by cookie) to the same connected client
+    // this mapping is established upon their first connection to the table,
+    // and for the host, established upon table creation.
+    readonly activeConnections: Map<string, ConnectedClient>;
+    // acitveConnections: { [key: string]: ConnectedClient };
 
-  readonly password: string;
-  // adminOptions
+    readonly password: string;
+    // adminOptions
 }
 
 // represents a person that has accessed the game via their browser.
 // can be playing or not playing (spectating or in game).
 
 export declare interface ConnectedClient {
-  readonly uuid: string;
-  readonly playerUUID: string;
-  readonly ws: WebSocket;
+    readonly uuid: string;
+    readonly playerUUID: string;
+    ws: WebSocket;
 }
 
 export declare interface NewGameForm {
-  gameType: GameType;
-  smallBlind: number;
-  bigBlind: number;
-  password?: string;
-  adminOptions?: any;
+    gameType: GameType;
+    smallBlind: number;
+    bigBlind: number;
+    password?: string;
+    adminOptions?: any;
 }
