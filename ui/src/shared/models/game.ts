649d0c456395b260d279d31ffd7a32e3ef91a673
jberman49 <jberman49@bloomberg.net>
2020-04-30 23:56:50 -0400

    add global game params

--- ui/src/shared/models/game.ts
+++ ui/src/shared/models/game.ts
@@ -1,76 +1,76 @@
 export declare interface BettingRoundAction {
-  type: BettingRoundActionType;
-  amount?: number;
+    type: BettingRoundActionType;
+    amount?: number;
 }
 
 // TODO re-evaluate semantics of WSAction vs BettingRoundAction vs lastActionType of Player
 // and see if you can find a better design
-export const enum BettingRoundActionType {
-  // distinction between bet and call may be unnecessary
-  CALL = "CALL",
-  CHECK = "CHECK",
-  BET = "BET",
-  // distinction between bet and raise may be unnecessary
-  // RAISE = 'RAISE',
-  FOLD = "FOLD",
-  WAITING_TO_ACT = "WAITING_TO_ACT",
-  NOT_IN_HAND = "NOT_IN_HAND",
-  ALL_IN = "ALL_IN",
-  PLACE_BLIND = "PLACE_BLIND",
+export enum BettingRoundActionType {
+    // distinction between bet and call may be unnecessary
+    CALL = "CALL",
+    CHECK = "CHECK",
+    BET = "BET",
+    // distinction between bet and raise may be unnecessary
+    // RAISE = 'RAISE',
+    FOLD = "FOLD",
+    WAITING_TO_ACT = "WAITING_TO_ACT",
+    NOT_IN_HAND = "NOT_IN_HAND",
+    ALL_IN = "ALL_IN",
+    PLACE_BLIND = "PLACE_BLIND",
 }
 
 export declare interface GameParameters {
-  smallBlind: number;
-  bigBlind: number;
-  gameType: GameType;
-  timeToAct: number;
-  maxPlayers: number;
+    smallBlind: number;
+    bigBlind: number;
+    gameType: GameType;
+    timeToAct: number;
+    maxPlayers: number;
 }
 
-export const enum StraddleType {
-  NoStraddle = "NoStraddle",
-  MississipiStraddle = "MississipiStraddle",
-  NormalStraddle = "NormalStraddle",
+export enum StraddleType {
+    NoStraddle = "NoStraddle",
+    MississipiStraddle = "MississipiStraddle",
+    NormalStraddle = "NormalStraddle",
 }
 
-export const enum GameType {
-  LHOLDEM = "LHOLDEM",
-  NLHOLDEM = "NLHOLDEM",
-  PLOMAHA = "PLOMAHA",
+export enum GameType {
+    LHOLDEM = "LHOLDEM",
+    NLHOLDEM = "NLHOLDEM",
+    PLOMAHA = "PLOMAHA",
 }
 
 // The usage of this enum might need to change
 // if expanding app to beyond nlholdem and plo
-export const enum BettingRoundStage {
-  WAITING = "WAITING",
-  PREFLOP = "PREFLOP",
-  FLOP = "FLOP",
-  TURN = "TURN",
-  RIVER = "RIVER",
-  SHOWDOWN = "SHOWDOWN",
+export enum BettingRoundStage {
+    WAITING = "WAITING",
+    PREFLOP = "PREFLOP",
+    FLOP = "FLOP",
+    TURN = "TURN",
+    RIVER = "RIVER",
+    SHOWDOWN = "SHOWDOWN",
 }
 
 export const BETTING_ROUND_STAGES = [
-  BettingRoundStage.WAITING,
-  BettingRoundStage.PREFLOP,
-  BettingRoundStage.FLOP,
-  BettingRoundStage.TURN,
-  BettingRoundStage.RIVER,
-  BettingRoundStage.SHOWDOWN,
+    BettingRoundStage.WAITING,
+    BettingRoundStage.PREFLOP,
+    BettingRoundStage.FLOP,
+    BettingRoundStage.TURN,
+    BettingRoundStage.RIVER,
+    BettingRoundStage.SHOWDOWN,
 ];
 
 export const CHECK_ACTION: BettingRoundAction = {
-  type: BettingRoundActionType.CHECK,
+    type: BettingRoundActionType.CHECK,
 };
 
 export const FOLD_ACTION: BettingRoundAction = {
-  type: BettingRoundActionType.FOLD,
+    type: BettingRoundActionType.FOLD,
 };
 
 export const WAITING_TO_ACT: BettingRoundAction = {
-  type: BettingRoundActionType.WAITING_TO_ACT,
+    type: BettingRoundActionType.WAITING_TO_ACT,
 };
 
 export const CALL_ACTION: BettingRoundAction = {
-  type: BettingRoundActionType.CALL,
+    type: BettingRoundActionType.CALL,
 };
--- ui/src/store/selectors.tsx
+++ ui/src/store/selectors.tsx
@@ -8,7 +8,10 @@ import {
 export const tableSelector = (gs: UiGameState): Table => gs.table;
 export const controllerSelector = (gs: UiGameState): Controller =>
     gs.controller;
-export const heroIsSeatedSelector = (gs: UiGameState): boolean => gs.heroIsSeated;
+export const heroIsSeatedSelector = (gs: UiGameState): boolean => gs.global.heroIsSeated;
 export const playersSelector = (gs: UiGameState): Player[] => gs.players;
 export const heroHandLabelSelector = (gs: UiGameState): string =>
     (gs.players.find((p) => p.hero) || {}).handLabel || "";
+
+
+export const allowStraddleSelector = (gs: UiGameState): boolean => gs.global.allowStraddle;
