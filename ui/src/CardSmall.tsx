5c7f468fd906bc67c8a55163da332529f047f79f
jberman49 <jberman49@bloomberg.net>
2020-05-02 22:42:16 -0400

    add deal animaton

--- ui/src/CardSmall.tsx
+++ ui/src/CardSmall.tsx
@@ -1,43 +1,43 @@
-import React from "react";
+import React from 'react';
 
-import { generateStringFromSuit, SUITS } from "./utils";
-import classnames from "classnames";
+import { generateStringFromSuit, SUITS } from './utils';
+import classnames from 'classnames';
 
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         zIndex: -1,
         borderRadius: 6,
-        textAlign: "center",
-        position: "relative",
-        backgroundColor: "white",
-        height: "8.8vmin",
-        width: "6.8vmin",
-        display: "flex",
-        justifyContent: "space-evenly",
-        margin: "0 0.5vmin",
+        textAlign: 'center',
+        position: 'relative',
+        backgroundColor: 'white',
+        height: '8.8vmin',
+        width: '6.8vmin',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        margin: '0 0.5vmin',
     },
     text: {
-        letterSpacing: "-0.3vmin",
-        fontSize: "3vmin",
-        color: "white",
-        fontWeight: "bold",
-        marginTop: "18%",
+        letterSpacing: '-0.3vmin',
+        fontSize: '3vmin',
+        color: 'white',
+        fontWeight: 'bold',
+        marginTop: '18%',
     },
     hidden: {
-        margin: "0 0.5vmin",
+        margin: '0 0.5vmin',
         borderRadius: 6,
-        textAlign: "center",
-        position: "relative",
-        backgroundColor: "white",
-        height: "6.8vmin",
-        width: "5.5vmin",
-        display: "flex",
-        justifyContent: "center",
-        fontSize: "3vmin",
-        fontWeight: "bold",
+        textAlign: 'center',
+        position: 'relative',
+        backgroundColor: 'white',
+        height: '6.8vmin',
+        width: '5.5vmin',
+        display: 'flex',
+        justifyContent: 'center',
+        fontSize: '3vmin',
+        fontWeight: 'bold',
         ...theme.custom.HIDDEN,
     },
     [SUITS.HEARTS]: {
@@ -59,19 +59,11 @@ function CardSmall(props) {
     const { suit, rank, hidden, className } = props;
 
     if (hidden) {
-        return (
-            <Typography
-                className={classnames(classes.root, classes.hidden, className)}
-            >
-                JP
-            </Typography>
-        );
+        return <Typography className={classnames(classes.root, classes.hidden, className)}>JP</Typography>;
     }
     return (
         <div className={classnames(classes.root, classes[suit], className)}>
-            <Typography className={classnames(classes.text)}>
-                {`${rank} ${generateStringFromSuit(suit)}`}
-            </Typography>
+            <Typography className={classnames(classes.text)}>{`${rank} ${generateStringFromSuit(suit)}`}</Typography>
         </div>
     );
 }
