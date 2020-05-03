b2b8db9107ba01a36b249c9f86232f2c779dcd88
julesberman <julesmichaelberman@gmail.com>
2020-05-02 22:42:16 -0400

    add deal animaton

--- ui/src/Bet.tsx
+++ ui/src/Bet.tsx
@@ -1,23 +1,23 @@
-import React, { Fragment } from "react";
-import get from "lodash/get";
-import classnames from "classnames";
-import ChipStack from "./ChipStack";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+import React, { Fragment } from 'react';
+import get from 'lodash/get';
+import classnames from 'classnames';
+import ChipStack from './ChipStack';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        position: "absolute",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
+        position: 'absolute',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
     },
     amount: {
-        color: "white",
-        fontWeight: "bold",
-        fontSize: "1.8vmin",
-        marginTop: "0.3vmin",
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: '1.8vmin',
+        marginTop: '0.3vmin',
     },
 }));
 
@@ -27,16 +27,13 @@ function Bet(props) {
 
     return (
         <div
-            className={classnames(classes.root, className, "bet")}
+            className={classnames(classes.root, className, 'ani_bet')}
             style={{
                 ...style,
             }}
         >
             <ChipStack amount={amount} />
-            <Typography
-                variant={"h6"}
-                className={classnames(classes.amount, "ani_betLabel")}
-            >
+            <Typography variant={'h6'} className={classnames(classes.amount, 'ani_betLabel')}>
                 {amount.toLocaleString()}
             </Typography>
         </div>
