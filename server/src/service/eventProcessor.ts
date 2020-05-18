f8e5931cc5b236960a47114924fc222bd8a9cb1b
julesberman <julesmichaelberman@gmail.com>
2020-05-18 14:20:11 -0400

    add comment

--- server/src/service/eventProcessor.ts
+++ server/src/service/eventProcessor.ts
@@ -16,8 +16,10 @@ export class EventProcessor {
 
         // either process via messageService or via stateGraphManager
         // now im thinking maybe the flow should be singlular, that is all events get processed through messageService
-        // timeouts can essentially look like another message from client or whatever
         // MessageService already acts as an event dispatcher, so its sort overly complicated to have two eventDisptachers
         // as long as every message contains a gameInstanceUUID, we can call this.gameInstanceManager.loadGameInstance(gameInstanceUUID), on Recieve of any message
+        // MessageService can be agnositc as to source of message, could be from client or from internal timeout
+        // we can refator timeoutManager so that it only dispatch events which can be processed by the message service rather an executing an aribiarty function
+        // this will ensure that the proper context switches happen for all queued timeouts
     }
 }
