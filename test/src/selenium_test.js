45fe20aa838420b9ba66a2f08ffd1611177eadaf
julesberman <julesmichaelberman@gmail.com>
2020-05-25 23:49:41 -0400

    refactor to be more modular

--- test/src/selenium_test.js
+++ test/src/selenium_test.js
@@ -7,6 +7,13 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// global params
+const windowSize = { x: 850, y: 600 }; // https://www.amazon.com/LG-27UD68-P-27-Inch-Monitor-FreeSync/dp/B01CH9ZTI4
+let curPos = { x: 0, y: 0 };
+const maxCols = 4;
+const allDrivers = [];
+
+// driver methods
 function spawnWindowAtLocation(url) {
   const driver = new webdriver.Builder().forBrowser("chrome").build();
   driver.get(url);
@@ -27,32 +34,12 @@ function spawnWindowAtLocation(url) {
   return driver;
 }
 
-// https://www.amazon.com/LG-27UD68-P-27-Inch-Monitor-FreeSync/dp/B01CH9ZTI4
-const windowSize = { x: 850, y: 600 };
-let curPos = { x: 0, y: 0 };
-const maxCols = 4;
-const allDrivers = [];
-
-async function gameWithNPlayers(numPlayers) {
-  const WaitBetweenRounds = 3200 + 125 * numPlayers;
-  const drivers = [];
-  const adminWindow = spawnWindowAtLocation("http://localhost:3000/");
-  drivers.push(adminWindow);
-  await adminWindow.findElement(By.id("ID_CreateGameButton")).click();
-  await sitDown(adminWindow, "ADMIN");
-  const gameUrl = adminWindow.getCurrentUrl();
-  for (let i = 1; i < numPlayers; i++) {
-    let newDriver = await spawnWindowAtLocation(gameUrl);
-    drivers.push(newDriver);
-    await sitDown(newDriver, `PLAYER_${i}`);
-  }
-
-  await adminWindow.findElement(By.id("ID_StartGameButton")).click();
-
+async function checkItDown(drivers) {
+  const WaitBetweenRounds = 3000 + 150 * drivers.length;
   // only do 100 checks
   for (let i = 1; i < 100; i++) {
-    for (driver of drivers) {
-      await checkCall(driver);
+    for (d of drivers) {
+      await checkCall(d);
     }
     await sleep(WaitBetweenRounds);
   }
@@ -60,20 +47,61 @@ async function gameWithNPlayers(numPlayers) {
 
 function sitDown(driver, playerName) {
   return driver
-    .findElement(By.className("CLASS_OpenSeatButton"))
-    .click()
-    .then(() => driver.findElement(By.id("ID_NameField")).sendKeys(playerName))
-    .then(() => driver.findElement(By.id("ID_SitDownButton")).click());
+    .wait(until.elementLocated(By.className("CLASS_OpenSeatButton")))
+    .then(() =>
+      driver
+        .findElement(By.className("CLASS_OpenSeatButton"))
+        .click()
+        .then(() =>
+          driver.findElement(By.id("ID_NameField")).sendKeys(playerName)
+        )
+        .then(() => driver.findElement(By.id("ID_SitDownButton")).click())
+    );
 }
 
 async function checkCall(driver) {
   return driver.findElement(By.id("ID_CheckCallButton")).click();
 }
 
-async function ThreeTablesThreePlayers() {
+// gameplay functions
+async function createGameSitDownAdmin() {
+  const adminWindow = spawnWindowAtLocation("http://localhost:3000/");
+  await adminWindow.findElement(By.id("ID_CreateGameButton")).click();
+  await sitDown(adminWindow, `ADMIN`);
+  return adminWindow;
+}
+
+async function sitDownPlayers(numPlayers, gameUrl) {
+  const drivers = [];
+  for (let i = 1; i < numPlayers; i++) {
+    let newDriver = await spawnWindowAtLocation(gameUrl);
+    drivers.push(newDriver);
+    await sitDown(newDriver, `PLAYER_${i}`);
+  }
+  return drivers;
+}
+
+async function createGameSitDownPlayers(numPlayers) {
+  const adminWindow = await createGameSitDownAdmin();
+  const gameUrl = adminWindow.getCurrentUrl();
+  const playerDrivers = await sitDownPlayers(numPlayers, gameUrl);
+  await adminWindow.findElement(By.id("ID_StartGameButton")).click();
+}
+
+async function playGameWithPlayers(numPlayers) {
+  const adminWindow = await createGameSitDownAdmin();
+  const gameUrl = adminWindow.getCurrentUrl();
+  const playerDrivers = await sitDownPlayers(numPlayers, gameUrl);
+  await adminWindow.findElement(By.id("ID_StartGameButton")).click();
+  checkItDown([adminWindow, ...playerDrivers]);
+}
+
+// main functions
+
+async function PlayThreeTablesThreePlayers() {
   try {
     await Promise.all[
-      (gameWithNPlayers(3), gameWithNPlayers(3), gameWithNPlayers(3))
+      (playGameWithPlayers(3), playGameWithPlayers(3), playGameWithPlayers(3))
     ];
   } catch (error) {
     console.log("caught err", error);
@@ -81,14 +109,24 @@ async function ThreeTablesThreePlayers() {
   }
 }
 
-async function OneTableNinePlayers() {
+async function PlayOneTableNinePlayers() {
   try {
-    await gameWithNPlayers(9);
+    await playGameWithPlayers(9);
   } catch (error) {
     console.log("caught err", error);
     allDrivers.forEach((d) => d.quit());
   }
 }
 
-ThreeTablesThreePlayers();
-// OneTableNinePlayers();
+async function SitDownThreePlayersOneTable() {
+  try {
+    await createGameSitDownPlayers(3);
+  } catch (error) {
+    console.log("caught err", error);
+    allDrivers.forEach((d) => d.quit());
+  }
+}
+
+// PlayThreeTablesThreePlayers();
+// PlayOneTableNinePlayers();
+SitDownThreePlayersOneTable();
