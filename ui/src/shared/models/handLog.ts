b38e00d60e4d67c632f0c73012fabe619ca369c6
Barney Rhodes <justpokerdev@gmail.com>
2020-05-27 12:20:38 -0400

    skeleton for interfaces

---
+++ ui/src/shared/models/handLog.ts
@@ -0,0 +1,15 @@
+export declare interface GameInstanceLog {
+
+}
+
+export declare interface HandRecord {
+
+}
+
+export declare interface HandAction {
+
+}
+
+export declare interface PlayerSummary {
+
+}
