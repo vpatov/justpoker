585d920f3b2965c704cfecf0bd73b400c1fd2924
Barney Rhodes <justpokerdev@gmail.com>
2020-05-29 13:45:03 -0400

    Implement interfaces for hand logs

--- ui/src/shared/models/handLog.ts
+++ ui/src/shared/models/handLog.ts
@@ -1,15 +1,53 @@
+import { PlayerUUID, GameInstanceUUID } from "./uuid";
+import { Card } from "./cards";
+import { BettingRoundAction, BettingRoundStage } from "./game";
+
+ enum PlayerPosition {
+     "SB" = "SB",
+     "BB" = "BB",
+     "UTG" = "UTG",
+     "UTG+1" = "UTG+1",
+     "UTG+2" = "UTG+2",
+     "MP" = "MP",
+     "MP+1" = "MP+1",
+     "LJ" = "LJ",
+     "HJ" = "HJ",
+     "CUTOFF" = "CUTOFF",
+     "BUTTON" = "BUTTON",
+ }
+
 export declare interface GameInstanceLog {
-
+    gameInstanceUUID: GameInstanceUUID;
 }
 
-export declare interface HandRecord {
-
+export declare interface LogOfHandRecord {
+    handNumber: number;
+    timeHandStarted: number;
+    allPlayers: PlayerUUID[];
+    winner: PlayerUUID;
+    actions: Map<BettingRoundStage, LogOfBettingRoundStage>;
+    lastStage: BettingRoundStage;
 }
 
-export declare interface HandAction {
-
+export declare interface LogOfBettingRoundStage {
+    cardsDealtThisStage: Card[];
+    board: Card[];
+    bettingRoundStage: BettingRoundStage;
+    handActions: LogOfHandAction[];
 }
 
-export declare interface PlayerSummary {
+export declare interface LogOfHandAction {
+    playerUUID: PlayerUUID;
+    bettingRoundAction: BettingRoundAction;
+    timeTookToAct: number;
+}
 
+export declare interface LogOfPlayerSummary {
+    playerUUID: PlayerUUID;
+    position: PlayerPosition;
+    wasDealtIn: boolean;
+    holeCards: Card[];
+    startingChips: number;
+    chipsDelta: number;
+    showedCards: boolean;
 }
