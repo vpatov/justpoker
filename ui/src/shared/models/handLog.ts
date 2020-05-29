d4fdf45eb9e5bc005ef669a79db5297ea13055b0
Barney Rhodes <justpokerdev@gmail.com>
2020-05-29 14:08:22 -0400

    Implement getClean functions in handLog.ts

--- ui/src/shared/models/handLog.ts
+++ ui/src/shared/models/handLog.ts
@@ -1,6 +1,6 @@
-import { PlayerUUID, GameInstanceUUID } from "./uuid";
+import { PlayerUUID, GameInstanceUUID, makeBlankUUID } from "./uuid";
 import { Card } from "./cards";
-import { BettingRoundAction, BettingRoundStage } from "./game";
+import { BettingRoundAction, BettingRoundStage, BettingRoundActionType } from "./game";
 
  enum PlayerPosition {
      "SB" = "SB",
@@ -14,35 +14,37 @@ import { BettingRoundAction, BettingRoundStage } from "./game";
      "HJ" = "HJ",
      "CUTOFF" = "CUTOFF",
      "BUTTON" = "BUTTON",
+     "NOT_PLAYING" = "NOT_PLAYING",
  }
 
 export declare interface GameInstanceLog {
     gameInstanceUUID: GameInstanceUUID;
+    handLogs: HandLog[];
 }
 
-export declare interface LogOfHandRecord {
+export declare interface HandLog {
     handNumber: number;
     timeHandStarted: number;
     allPlayers: PlayerUUID[];
     winner: PlayerUUID;
-    actions: Map<BettingRoundStage, LogOfBettingRoundStage>;
+    actions: Map<BettingRoundStage, BettingRoundStageLog>;
     lastStage: BettingRoundStage;
 }
 
-export declare interface LogOfBettingRoundStage {
+export declare interface BettingRoundStageLog {
     cardsDealtThisStage: Card[];
     board: Card[];
     bettingRoundStage: BettingRoundStage;
-    handActions: LogOfHandAction[];
+    handActions: HandActionLog[];
 }
 
-export declare interface LogOfHandAction {
+export declare interface HandActionLog {
     playerUUID: PlayerUUID;
     bettingRoundAction: BettingRoundAction;
     timeTookToAct: number;
 }
 
-export declare interface LogOfPlayerSummary {
+export declare interface PlayerSummaryForHandLog {
     playerUUID: PlayerUUID;
     position: PlayerPosition;
     wasDealtIn: boolean;
@@ -50,4 +52,51 @@ export declare interface LogOfPlayerSummary {
     startingChips: number;
     chipsDelta: number;
     showedCards: boolean;
+}
+
+export function getCleanGameInstanceLog(): GameInstanceLog {
+    return {
+        gameInstanceUUID: makeBlankUUID(),
+        handLogs: []
+    };
+}
+
+export function getCleanHandLog(): HandLog {
+    return {
+        handNumber: -1,
+        timeHandStarted: 0,
+        allPlayers: [],
+        winner: makeBlankUUID(),
+        actions: new Map(),
+        lastStage: BettingRoundStage.WAITING,
+    };
+}
+
+export function getCleanBettingRoundStageLog(): BettingRoundStageLog {
+    return {
+        cardsDealtThisStage: [],
+        board: [],
+        bettingRoundStage: BettingRoundStage.WAITING,
+        handActions: []
+    };
+}
+
+export function getCleanHandActionLog(): HandActionLog {
+    return {
+        playerUUID: makeBlankUUID(),
+        bettingRoundAction: {type: BettingRoundActionType.NOT_IN_HAND},
+        timeTookToAct: 0,
+    };
+}
+
+export function getCleanPlayerSummaryForHandLog(): PlayerSummaryForHandLog {
+    return {
+        playerUUID: makeBlankUUID(),
+        position: PlayerPosition.NOT_PLAYING,
+        wasDealtIn: false,
+        holeCards: [],
+        startingChips: 0,
+        chipsDelta: 0,
+        showedCards: false,
+    };
 }
