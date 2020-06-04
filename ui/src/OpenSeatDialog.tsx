8665cc0d7d593d47dcc73f197294b9fcca778292
julesberman <julesmichaelberman@gmail.com>
2020-06-03 21:57:18 -0400

    refactor out openseatdialog

---
+++ ui/src/OpenSeatDialog.tsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { WsServer } from './api/ws';
+import { ClientActionType } from './shared/models/api';
+import TextFieldWrap from './reuseable/TextFieldWrap';
+
+import { makeStyles } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
+import { ClientWsMessageRequest } from './shared/models/api';
+import { Dialog, DialogContent, DialogActions, Button } from '@material-ui/core';
+import { selectGameParameters } from './store/selectors';
+
+const useStyles = makeStyles((theme) => ({
+    field: {
+        marginTop: '3vmin',
+        width: '35%',
+    },
+    sit: {
+        fontSize: '1.3vmin',
+    },
+    dialogPaper: {
+        height: '80vh',
+        maxHeight: 360,
+    },
+}));
+
+const NAME_LOCAL_STORAGE_KEY = 'jp-last-used-name';
+
+function OpenSeatDialog(props) {
+    const classes = useStyles();
+    const { onClose, open, seatNumber } = props;
+    const [name, setName] = useState(localStorage.getItem(NAME_LOCAL_STORAGE_KEY) || '');
+    const { maxBuyin, minBuyin } = useSelector(selectGameParameters);
+
+    const [buyin, setBuyin] = useState<number | undefined>();
+
+    function onChangeBuyin(event: any) {
+        setBuyin(event.target.value);
+    }
+
+    function invalidBuyin() {
+        if (buyin === undefined) {
+            return false;
+        }
+        return buyin < minBuyin || buyin > maxBuyin;
+    }
+
+    function invalidName() {
+        return name === '';
+    }
+
+    function formInvalid() {
+        return invalidBuyin() || invalidName() || buyin === undefined;
+    }
+
+    function onSubmitSitDownForm() {
+        WsServer.send({
+            actionType: ClientActionType.JOINTABLEANDSITDOWN,
+            request: {
+                name,
+                buyin: Number(buyin),
+                seatNumber: seatNumber,
+            } as ClientWsMessageRequest,
+        });
+        onClose();
+    }
+
+    function onPressEnter(event: any) {
+        if (event.key === 'Enter' && !formInvalid()) {
+            event.preventDefault();
+            onSubmitSitDownForm();
+        }
+    }
+
+    return (
+        <Dialog
+            open={open}
+            maxWidth="xs"
+            fullWidth
+            onKeyPress={(event) => onPressEnter(event)}
+            classes={{ paper: classes.dialogPaper }}
+        >
+            <DialogContent>
+                <TextFieldWrap
+                    autoFocus
+                    id="ID_NameField"
+                    label="Name"
+                    type="text"
+                    fullWidth
+                    onChange={(event) => {
+                        setName(event.target.value);
+                        localStorage.setItem(NAME_LOCAL_STORAGE_KEY, event.target.value);
+                    }}
+                    value={name}
+                    variant="standard"
+                    maxChars={24}
+                />
+                <TextFieldWrap
+                    variant="standard"
+                    type="number"
+                    className={classes.field}
+                    value={buyin}
+                    label="Buy In"
+                    onChange={onChangeBuyin}
+                    inputProps={{
+                        step: 1,
+                    }}
+                    max={maxBuyin}
+                    error={invalidBuyin()}
+                    helperText={invalidBuyin() ? `Min Buyin is ${minBuyin}` : ''}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClose}>Cancel</Button>
+                <Button id="ID_SitDownButton" disabled={formInvalid()} onClick={onSubmitSitDownForm} color="primary">
+                    Sit Down
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default OpenSeatDialog;
