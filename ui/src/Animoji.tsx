412e0eb24343fbd2e926edce745d833f82dac5ab
julesberman <julesmichaelberman@gmail.com>
2020-06-05 12:11:22 -0400

    add shadow to aniemoji

--- ui/src/Animoji.tsx
+++ ui/src/Animoji.tsx
@@ -9,6 +9,7 @@ const useStyles = makeStyles((theme: Theme) =>
         img: {
             width: `100%`,
             height: `100%`,
+            filter: `drop-shadow(0px 0px 3px rgba(0, 0, 0, .7))`,
         },
     }),
 );
