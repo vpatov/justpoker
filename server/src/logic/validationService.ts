1bb3234bd8b1c20286ff6fff6d4762753b0df129
Barney Rhodes <justpokerdev@gmail.com>
2020-06-15 17:24:02 -0400

    Add validation that ensures sit in/out is not processed during show start of hand and show start of betting round stages

--- server/src/logic/validationService.ts
+++ server/src/logic/validationService.ts
@@ -18,6 +18,7 @@ import { Card, cardsAreEqual } from '../../../ui/src/shared/models/game/cards';
 import { debugFunc } from '../logger';
 import { MIN_VALUES, MAX_VALUES } from '../../../ui/src/shared/util/consts';
 import { errorMonitor } from 'events';
+import { GameStage } from '../../../ui/src/shared/models/game/stateGraph';
 
 const MAX_NAME_LENGTH = 32;
 
@@ -228,8 +229,18 @@ export class ValidationService {
         return this.ensurePlayerIsAtTable(player.uuid);
     }
 
+    validateSitInOrOutAction(clientUUID: ClientUUID): ValidationResponse {
+        const error = this.ensureClientIsInGame(clientUUID);
+        if (error) return error;
+
+        return this.validateNotInGameStages(
+            [GameStage.SHOW_START_OF_HAND, GameStage.SHOW_START_OF_BETTING_ROUND],
+            'sit in/out',
+        );
+    }
+
     validateSitInAction(clientUUID: ClientUUID): ValidationResponse {
-        let error = this.ensureClientIsInGame(clientUUID);
+        let error = this.validateSitInOrOutAction(clientUUID);
         if (error) return error;
 
         const player = this.gsm.getPlayerByClientUUID(clientUUID);
@@ -247,12 +258,22 @@ export class ValidationService {
     }
 
     validateSitOutAction(clientUUID: ClientUUID): ValidationResponse {
-        const error = this.ensureClientIsInGame(clientUUID);
-        if (error) {
-            return error;
-        }
+        const error = this.validateSitInOrOutAction(clientUUID);
+        if (error) return error;
+
         const player = this.gsm.getPlayerByClientUUID(clientUUID);
         return this.ensurePlayerIsSittingIn(player.uuid);
+    }
+
+    validateNotInGameStages(stages: GameStage[], actionString?: string): ValidationResponse {
+        const stage = this.gsm.getGameStage();
+        if (stages.indexOf(stage) > -1) {
+            return {
+                errorType: ErrorType.ILLEGAL_ACTION,
+                errorString: `Cannot perform ${actionString || 'action'} during gamestage ${stage}`,
+            };
+        }
+        return undefined;
     }
 
     validateStartGameRequest(clientUUID: ClientUUID): ValidationResponse {
