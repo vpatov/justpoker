595d7316f1efca0704c98d1f6ab046045e4daea2
julesberman <julesmichaelberman@gmail.com>
2020-06-16 01:27:19 -0400

    add border

--- ui/src/table/EmptySeat.tsx
+++ ui/src/table/EmptySeat.tsx
@@ -9,28 +9,31 @@ const size = 5;
 
 const useStyles = makeStyles((theme) => ({
     hoverZone: {
-        width: `${size*2}vmin`,
-        height: `${size*4}vmin`,
+        width: `${size * 2}vmin`,
+        height: `${size * 2}vmin`,
         borderRadius: '50%',
         justifyContent: 'center',
         alignItems: 'center',
         '&:hover button': {
             visibility: 'visible',
         },
+        '&:hover ': {
+            border: '0.2vmin solid white',
+        },
     },
     container: {
         display: 'flex',
         flexDirection: 'column',
-        clipPath: `circle(${size}vmin at ${size}vmin ${size}vmin)`
+        clipPath: `circle(${size}vmin at ${size}vmin ${size}vmin)`,
     },
     button: {
         borderRadius: 'unset',
-        width: `${size*2}vmin`,
+        width: `${size * 2}vmin`,
         height: `${size}vmin`,
         visibility: 'hidden',
     },
     topButtonText: {
-        marginTop: '1vmin'
+        marginTop: '1vmin',
     },
     bottomButtonText: {
         marginBottom: '1vmin',
@@ -40,28 +43,26 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-
-
 function EmptySeat(props) {
     const classes = useStyles();
     const { className, style, setHeroRotation, virtualPositon } = props;
 
     function handleRotationButtonClick() {
         setHeroRotation(virtualPositon);
-    };
+    }
 
     function handleSeatChangeClick() {
-        // TODO implementing 
+        // TODO implementing
     }
 
     return (
         <div className={classnames(classes.hoverZone, className)} style={style}>
             <div className={classes.container}>
                 <IconButton className={classnames(classes.button)} onClick={handleRotationButtonClick}>
-                    <Typography className={classnames(classes.text,classes.topButtonText)}>Rotate Here</Typography>
+                    <Typography className={classnames(classes.text, classes.topButtonText)}>Rotate Here</Typography>
                 </IconButton>
                 <IconButton className={classnames(classes.button)} onClick={handleSeatChangeClick}>
-                    <Typography className={classnames(classes.text,classes.bottomButtonText)}>Sit Here</Typography>
+                    <Typography className={classnames(classes.text, classes.bottomButtonText)}>Sit Here</Typography>
                 </IconButton>
             </div>
         </div>
