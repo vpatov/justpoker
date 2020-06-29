e148506df13d7dd2ded186d01611a890a6261a95
Barney Rhoades <justpokerdev@gmail.com>
2020-06-29 12:16:53 -0400

    add download button + functionality to handlog

--- ui/src/api/http.ts
+++ ui/src/api/http.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { CONFIGS, Config, ENVIRONMENT } from '../shared/models/config/config';
 
 const api = axios.create({
     baseURL: '', // for local dev currenly handled by proxy in package.json
 });
+
+const config: Config = process.env.REACT_APP_ENVIRONMENT === ENVIRONMENT.PROD ? CONFIGS.PROD : CONFIGS.DEV;
 
 // onSuccess = (response) => {}
 // onError = (err) => {}
@@ -15,6 +18,10 @@ export const getLedger = (gameInstanceUUID, onSuccess, onError) => {
     const url = `/api/ledger?gameInstanceUUID=${gameInstanceUUID}`;
     return api.get(url).then(onSuccess).catch(onError);
 };
+
+export function computeHandLogGETurl(gameInstanceUUID) {
+    return `http${config.HTTPS ? 's' : ''}://${config.SERVER_URL}:${config.SERVER_PORT}/api/handlog?gameInstanceUUID=${gameInstanceUUID}`;
+}
 
 export const reportFrontEndError = (data, onSuccess, onError) => {
     const url = `/api/error`;
